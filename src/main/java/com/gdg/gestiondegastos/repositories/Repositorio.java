647b3d6aadcc6c665288ee2cdc010da4d1e15164
Jochemali <gimenezleon98@gmail.com>
2021-09-14 13:01:41 +0200

    conexion a ddbb correcta

--- src/main/java/com/gdg/gestiondegastos/repositories/Repositorio.java
+++ src/main/java/com/gdg/gestiondegastos/repositories/Repositorio.java
@@ -3,5 +3,5 @@ package com.gdg.gestiondegastos.repositories;
 import com.gdg.gestiondegastos.entities.Entidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Repositorio extends JpaRepository<Entidad, Integer> {
+public interface Repositorio{
 }

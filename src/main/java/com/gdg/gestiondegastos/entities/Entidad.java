a30354109eb6ea1476d58e911d894d1efcced5e1
Jochemali <gimenezleon98@gmail.com>
2021-09-14 12:37:54 +0200

    datos introducidos en application.properties (cambiar por vuestro puerto y nombre de base de datos)

--- src/main/java/com/gdg/gestiondegastos/entities/Entidad.java
+++ src/main/java/com/gdg/gestiondegastos/entities/Entidad.java
@@ -7,12 +7,7 @@ import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "Por definir")
-@Data
-@NoArgsConstructor
 public class Entidad {
 
-    @Id
     private Long id;
 }

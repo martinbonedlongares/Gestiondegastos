e734173393ebb709ffb49c25ec92306ca700d4af
Jochemali <gimenezleon98@gmail.com>
2021-09-15 16:42:20 +0200

    Arreglada la database para que nada mas ejecutar cree las tablas e inserte los datos

--- src/main/java/com/gdg/gestiondegastos/repositories/PresupuestoRepository.java
+++ src/main/java/com/gdg/gestiondegastos/repositories/PresupuestoRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PresupuestoRepository extends JpaRepository<Presupuesto,Integer>{
     
     
-     public Optional<Presupuesto> findByIdGrupo(Integer idGrupo); 
+     //public Optional<Presupuesto> findByIdGrupo(Integer idGrupo); 
      
      
 }

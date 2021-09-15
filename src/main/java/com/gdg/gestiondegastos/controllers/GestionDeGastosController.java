5695c5f65f5eb96cf56ab3e90e2b53e2919feabd
Jochemali <gimenezleon98@gmail.com>
2021-09-15 10:17:15 +0200

    Ultimo commit a mi rama, me paso a la main

---
+++ src/main/java/com/gdg/gestiondegastos/controllers/GestionDeGastosController.java
@@ -0,0 +1,30 @@
+package com.gdg.gestiondegastos.controllers;
+
+import com.gdg.gestiondegastos.repositories.GrupoRepository;
+import com.gdg.gestiondegastos.repositories.MovimientosRepository;
+import com.gdg.gestiondegastos.repositories.PresupuestoRepository;
+import com.gdg.gestiondegastos.repositories.UsuarioGrupoRepository;
+import com.gdg.gestiondegastos.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class GestionDeGastosController {
+
+    @Autowired
+    private UsuarioRepository repoUsuario;
+    @Autowired
+    private GrupoRepository repoGrupo;
+    @Autowired
+    private UsuarioGrupoRepository repoUsuarioGrupo;
+    @Autowired
+    private PresupuestoRepository repoPresupuesto;
+    @Autowired
+    private MovimientosRepository repoMovimientos;
+    
+    
+
+}

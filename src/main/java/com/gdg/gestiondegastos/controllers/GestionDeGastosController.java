8b8ea8e26520c85e8c2e6ca45b9c5d1b2cb32d84
Jochemali <gimenezleon98@gmail.com>
2021-09-15 12:16:51 +0200

    Creado controlador para guardar usuario

--- src/main/java/com/gdg/gestiondegastos/controllers/GestionDeGastosController.java
+++ src/main/java/com/gdg/gestiondegastos/controllers/GestionDeGastosController.java
@@ -1,5 +1,6 @@
 package com.gdg.gestiondegastos.controllers;
 
+import com.gdg.gestiondegastos.entities.Usuario;
 import com.gdg.gestiondegastos.repositories.GrupoRepository;
 import com.gdg.gestiondegastos.repositories.MovimientosRepository;
 import com.gdg.gestiondegastos.repositories.PresupuestoRepository;
@@ -7,7 +8,8 @@ import com.gdg.gestiondegastos.repositories.UsuarioGrupoRepository;
 import com.gdg.gestiondegastos.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,5 +28,12 @@ public class GestionDeGastosController {
     private MovimientosRepository repoMovimientos;
     
     
+    
+    @PostMapping("/agregar")
+    public String agregarUsuario(Model m, Usuario usuario){
+        repoUsuario.save(usuario);
+        
+        return "login";
+    }
 
 }
---
+++ src/main/resources/db/gestiondegastos/import.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Joche
+ * Created: 15 sept. 2021
+ */
+INSERT into usuario (nombre,contrasenya,correo,telefono) values ('joche','1111','jochemali@mail.com','646582521');

46234d3c99604e1a6f96138db8745e42c8df8595
Jochemali <gimenezleon98@gmail.com>
2021-09-20 13:34:42 +0200

    borrar usuario implementado, vista de usuarios ded un grupo implementada

--- src/main/java/com/gdg/gestiondegastos/controllers/GestionDeGastosController.java
+++ src/main/java/com/gdg/gestiondegastos/controllers/GestionDeGastosController.java
@@ -12,6 +12,7 @@ import com.gdg.gestiondegastos.repositories.UsuarioGrupoRepository;
 import com.gdg.gestiondegastos.repositories.UsuarioRepository;
 import com.mysql.cj.Constants;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,7 +40,7 @@ public class GestionDeGastosController {
     private MovimientosRepository repoMovimientos;
     // @Autowired
     // private ModelMapper obj;
-   // @Autowired
+    // @Autowired
     //private PasswordEncoder clave;
 
     // Este es un get para ver la principal y así ver los cambios
@@ -47,13 +48,22 @@ public class GestionDeGastosController {
     public String principal() {
         return "principal";
     }
-    
+
     @GetMapping("/inicio/{idUsuario}")
-    public String inicio(Model m,@PathVariable Integer idUsuario) {
-        Usuario user = repoUsuario.getById(idUsuario);
+    public String inicio(Model m, @PathVariable Integer idUsuario) {
+        Usuario user = repoUsuario.findById(idUsuario).get();
+        //user = repoUsuario.getById(idUsuario);
+
+        //Suma todas las cantidades iniciales indicadas en el presupuesto del usuario 
+        m.addAttribute("presupuestoPersonal", user.getUsuarioGrupo()
+                .stream().map(x -> x.getGrupo().getPresupuesto())
+                .collect(
+                        Collectors.summingDouble(
+                                p -> p.stream()
+                                        .collect(Collectors.summingDouble(z -> z.getCantidadInicio())))));
         
-        m.addAttribute("presupuestoPersonal", user.getId());
-       
+         m.addAttribute("movimientos", repoMovimientos.leerPorUsuario(idUsuario));
+
         return "principal";
     }
 
@@ -66,7 +76,7 @@ public class GestionDeGastosController {
 
     @GetMapping("/principal")
     public String principal(Model m) {
-       // m.addAttribute("usuario", new Usuario());
+        // m.addAttribute("usuario", new Usuario());
         return "login";
     }
 
@@ -81,67 +91,48 @@ public class GestionDeGastosController {
     @PostMapping("/ingresar") // hacer login
     public String ingresar(Model m, String username, String password) {
 
-       Usuario usuario =   new Usuario();
-       System.out.println(" USUARIO  1    "  + username);
-       try{        
-              usuario = repoUsuario.findByCorreo(username);
-              System.out.println(" USUARIO   2   "  + usuario.getNombre());
-       }catch (Exception e)
-       { e.printStackTrace();}
-           
-       if (usuario.getNombre() !=null )
-       
-     // if (usuario.getContrasenya()== clave.encode(password))
-       {
-           return "principal";
-      }
-      else {
-         return "login";
-      }
+        Usuario usuario = new Usuario();
+        System.out.println(" USUARIO  1    " + username);
+        try {
+            usuario = repoUsuario.findByCorreo(username);
+            System.out.println(" USUARIO   2   " + usuario.getNombre());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (usuario.getNombre() != null) // if (usuario.getContrasenya()== clave.encode(password))
+        {
+            return "principal";
+        } else {
+            return "login";
+        }
     }
 
     @GetMapping("/grupo/{idGrupo}")
-    public String verGrupos(Model m, @PathVariable Integer idGrupo) {        
+    public String verGrupos(Model m, @PathVariable Integer idGrupo) {
 
         m.addAttribute("grupo", repoGrupo.findById(idGrupo).get());
         m.addAttribute("movimientos", repoMovimientos.leerPorGrupo(idGrupo));
-        
+
         m.addAttribute("presupuesto", repoPresupuesto.findByIdGrupo(idGrupo));
 
         return "grupos";
     }
-    
-     @GetMapping("/grupo/{idGrupo}/gestionar")
-    public String gestionarGrupos(Model m, @PathVariable Integer idGrupo) {        
+
+    @GetMapping("/grupo/{idGrupo}/gestionar")
+    public String gestionarGrupos(Model m, @PathVariable Integer idGrupo) {
 
         m.addAttribute("grupos", repoGrupo.findById(idGrupo).get().getUsuarioGrupo());
-            
+
         return "gestionGrupos";
     }
 
     @GetMapping("/grupo/{idGrupo}/borrarUsuario")
-    public String borrarUsuario(Integer idUsuarioGrupo, Integer idGrupo){
+    public String borrarUsuario(Integer idUsuarioGrupo, Integer idGrupo) {
         repoUsuarioGrupo.deleteById(idUsuarioGrupo);
         return "redirect:/gestion/grupo/{idGrupo}/gestionar";
     }
-    
-    
-    /*@GetMapping("/movimientos")
-    public String verMovimientos(Model m, Integer idMovimiento) {
-        m.addAttribute("movimiento", repoMovimientos.findById(idMovimiento).get());
 
-        return "movimientos";
-    }*/
-
-    /*
-     * @PostMapping("/grupo/{idGrupo}/nuevoMovimiento") public String
-     * nuevoMovimientos(Model m, Integer idUsuarioGrupo){ Movimiento mov = new
-     * Movimiento();
-     * mov.setUsuarioGrupo(repoUsuarioGrupo.findById(idUsuarioGrupo).get());
-     * m.addAttribute("movimiento", mov);
-     * 
-     * return "nuevoMov"; }
-     */
     // Ejemplo ded url: http://localhost:8080/gestion/grupo/6
     @GetMapping("/grupo/{idGrupo}/nuevoMovimiento")
     public String nuevoMovimientos(Model m, Integer idUsuarioGrupo) {
@@ -158,9 +149,9 @@ public class GestionDeGastosController {
         UsuarioGrupo ug = repoUsuarioGrupo.findById(idUsuarioGrupo).get();
         mov.setUsuarioGrupo(ug);
         repoMovimientos.save(mov);
-        
+
         Presupuesto p = repoPresupuesto.findByIdGrupo(idGrupo);
-        p.setCantidadFinal(p.getCantidadFinal()+mov.getCantidad());
+        p.setCantidadFinal(p.getCantidadFinal() + mov.getCantidad());
         repoPresupuesto.save(p);
         return "redirect:/gestion/grupo/{idGrupo}";
     }

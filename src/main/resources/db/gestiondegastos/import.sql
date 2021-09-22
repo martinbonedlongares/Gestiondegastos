ba110a51c9b2402f14645725458f34ff5c4ceb39
Usuario <Usuario@192.168.160.39>
2021-09-22 12:06:32 +0200

    BBDD Modo Oscuro

--- src/main/resources/db/gestiondegastos/import.sql
+++ src/main/resources/db/gestiondegastos/import.sql
@@ -1,11 +1,11 @@
 
 --USUARIOS
 
-INSERT INTO usuario (nombre,contrasenya,correo,telefono) VALUES ('Raul','$2a$10$PkHebxbh3IMniYV09VtQNu32xDLX8yZLkO0JX1/GNqjvyR0wMY7JS','RaulHiberus@gmail.com','665379845');
-INSERT INTO usuario (nombre,contrasenya,correo,telefono) VALUES ('Diego','$2a$10$YQAzwUghK2aSkISs3ok3Cu4Ha4gcebPisgp6qlOcUjvpI4lbfr2QK','DiegoHiberus@gmail.com','655352261');
-INSERT INTO usuario (nombre,contrasenya,correo,telefono) VALUES ('Sofia','$2a$10$Vj6jJOvVEtWqkIBWx5KIjuB3hkIcc0567UUblBbnoPXCM4n1mMMe6','SofiaHiberus@gmail.com','722161974');
-INSERT INTO usuario (nombre,contrasenya,correo,telefono) VALUES ('Martin','$2a$10$H3yBhQpiyjqG02boZPcwlu7l5Je3Bu0E6N6mFb0Td4lxQT0GtyXW2','MartinHiberus@gmail.com','677826971');
-INSERT INTO usuario (nombre,contrasenya,correo,telefono) VALUES ('Jose María','$2a$10$R/YwN2b8AKfwxu0upaIA/Onufw7A5wCKZfgDc87sIFuFKdn8CSt26','JosemaHiberus@gmail.com','646582521');
+INSERT INTO usuario (nombre,contrasenya,correo,telefono,modoOscuro) VALUES ('Raul','$2a$10$PkHebxbh3IMniYV09VtQNu32xDLX8yZLkO0JX1/GNqjvyR0wMY7JS','RaulHiberus@gmail.com','665379845',1);
+INSERT INTO usuario (nombre,contrasenya,correo,telefono,modoOscuro) VALUES ('Diego','$2a$10$YQAzwUghK2aSkISs3ok3Cu4Ha4gcebPisgp6qlOcUjvpI4lbfr2QK','DiegoHiberus@gmail.com','655352261',0);
+INSERT INTO usuario (nombre,contrasenya,correo,telefono,modoOscuro) VALUES ('Sofia','$2a$10$Vj6jJOvVEtWqkIBWx5KIjuB3hkIcc0567UUblBbnoPXCM4n1mMMe6','SofiaHiberus@gmail.com','722161974',1);
+INSERT INTO usuario (nombre,contrasenya,correo,telefono,modoOscuro) VALUES ('Martin','$2a$10$H3yBhQpiyjqG02boZPcwlu7l5Je3Bu0E6N6mFb0Td4lxQT0GtyXW2','MartinHiberus@gmail.com','677826971',1);
+INSERT INTO usuario (nombre,contrasenya,correo,telefono,modoOscuro) VALUES ('Jose María','$2a$10$R/YwN2b8AKfwxu0upaIA/Onufw7A5wCKZfgDc87sIFuFKdn8CSt26','JosemaHiberus@gmail.com','646582521',0);
 
 
 --#GRUPOS#
@@ -42,29 +42,52 @@ INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALU
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-40,'Ropa','Pantalones Talla L y Camisa',CURDATE(),4);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-10,'Transporte','Recarga de tarjeta bus',CURDATE(),5);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+30,'Regalo','Propina de la abuela <3',CURDATE(),6);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (1.5,'Transporte','Monedas en el suelo, que suerte',CURDATE(),7);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-11.9,'Regalo','Libro de Stephen King para Paloma',CURDATE(),8);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-1.8,'Alimentacion','Cafe con churros en Hiberus',CURDATE(),9);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+4.5,'Hogar','Deuda de mi hermano saldada',CURDATE(),10);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-45.5,'Entretenimiento','Cascos New Skill Kimera V2',CURDATE(),6);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (1.50,'Transporte','Monedas en el suelo, que suerte',CURDATE(),7);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-11.90,'Regalo','Libro de Stephen King para Paloma',CURDATE(),8);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-1.80,'Alimentacion','Cafe con churros en Hiberus',CURDATE(),9);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+4.50,'Hogar','Deuda de mi hermano saldada',CURDATE(),10);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-45.50,'Entretenimiento','Cascos New Skill Kimera V2',CURDATE(),6);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+600.65,'Comunicacion','Resolución juicio del bar',CURDATE(),6);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-40.32,'Higiene','Productos rollo cremas, pasta de dientes etc',CURDATE(),8);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-0.9,'Entretenimiento','Karlson, oh... No sabes lo que es Karlson? Karlson es un...',CURDATE(),7);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-61.2,'Alimentación','Liquidos de vaper',CURDATE(),9);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-0.90,'Entretenimiento','Karlson, oh... No sabes lo que es Karlson? Karlson es un...',CURDATE(),7);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-61.20,'Alimentación','Liquidos de vaper',CURDATE(),9);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+120,'Regalo','Cumple YaY <3',CURDATE(),10);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-0.1,'Alimentacion','Propina del café',CURDATE(),10);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-102.8,'Comunicación','Factura Orange',CURDATE(),9);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-20.0,'Transporte','Gasolina',CURDATE(),5);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+5.5,'Regalo','Tarjeta reagalo hipercor',CURDATE(),6);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-0.10,'Alimentacion','Propina del café',CURDATE(),10);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-102.80,'Comunicación','Factura Orange',CURDATE(),9);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-20,'Transporte','Gasolina',CURDATE(),5);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+5.50,'Regalo','Tarjeta reagalo hipercor',CURDATE(),6);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-60,'Alimentacion','Carro de la compra',CURDATE(),7);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-100,'Alimentacion','Café con "descuento" de Hiberus',CURDATE(),10);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+20,'Higiene','Arreglar tuberia',CURDATE(),9);
 INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-3.00,'Transporte','Mas viajes de tranvia T.T',CURDATE(),8);
-INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-30.5,'Alimentacion','Cena UDON',CURDATE(),6);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-30.50,'Alimentacion','Cena UDON',CURDATE(),6);
 
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-30.50,'Higiene','Crema hemorroidal',CURDATE(),1);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+30.50,'Higiene','Cancelación "Crema hemorroidal"',CURDATE(),1);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (5,'Regalo','Reintegro cupon ONCE',CURDATE(),1);
+
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-20.50,'Entretenimiento','Dark Souls REMASTERED',CURDATE(),2);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (1000,'Hogar','Beca grado NICE',CURDATE(),2);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-60,'Transporte','Bono tranvia TUZSA',CURDATE(),2);
+
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-15.70,'Medicina','Pastillas, vitaminas etc',CURDATE(),3);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-1.50,'Transporte','Viaje bus sencillo',CURDATE(),3);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (+250.1,'Medicina','PLUS lesiones de servicio',CURDATE(),3);
+
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-300,'Hogar','Frigorifico antifrost',CURDATE(),4);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-4.5,'Hogar','FLEXICINTA',CURDATE(),4);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (100,'Comunicacion','Indemnización toxicidad FLEXICINTA',CURDATE(),4);
+
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (120,'Mascota','Concurso de belleza Perro',CURDATE(),5);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-25,'Mascota','Miscelánea para mi doggo',CURDATE(),5);
+INSERT INTO movimiento (cantidad,categoria,concepto,fecha,id_usuario_grupo) VALUES (-100,'Comunicacion','Daños y perjuicios FLEXICINTA',CURDATE(),5);
 
 --#PRESUPUESTOS#
-
-INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (100,150,"2021-09-15","2021-09-22",1);
-INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (1000,2000,"2021-09-15","2021-09-29",6);
-INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (500,1500,"2021-09-15","2021-09-29",7); 
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (100,55,"2021-09-15","2021-09-22",1);
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (220,1130.5,"2021-09-15","2021-09-22",2);
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (200,1633.65,"2021-09-15","2021-09-22",3);
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (300.50,56,"2021-09-15","2021-09-22",4);
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (355,340,"2021-09-15","2021-09-22",5);
+--Calculos para cantidad_final en revisión (si no sabes a que me refiero, no deberias estar aqui :<D)
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (1000,1433.13,"2021-09-15","2021-09-29",6);
+INSERT INTO presupuesto (cantidad_inicio,cantidad_final,fecha_inicio,fecha_final,id_grupo) VALUES (185.5,76.1,"2021-09-15","2021-09-29",7); 

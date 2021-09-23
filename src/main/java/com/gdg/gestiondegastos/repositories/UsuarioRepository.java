be41d7139d175c2d5b10c4d6ef4f51e1111305d1
Usuario <Usuario@192.168.160.39>
2021-09-23 13:11:58 +0200

    Usuariorepository con "updateContrasenya"

--- src/main/java/com/gdg/gestiondegastos/repositories/UsuarioRepository.java
+++ src/main/java/com/gdg/gestiondegastos/repositories/UsuarioRepository.java
@@ -2,9 +2,25 @@ package com.gdg.gestiondegastos.repositories;
 
 import com.gdg.gestiondegastos.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer>{
      public Usuario findByCorreo( @Param("correo") String correo);
      //Optional<Usuario> findUserByUsername(String username);
+    
+     
+     
+    @Modifying
+    @Query("update Usuario u set u.contrasenya = :contrasenya where u.id = :id_usuario")
+    public void updateContrasenya(Integer id_usuario, String contrasenya);
+    
+    
+    //BORRA DE LA BBDD, NO DEL GRUPO
+    @Modifying
+    @Query("delete from Usuario u where u.id = :id_usuario")
+    public void borrarUsuario(Integer id_usuario);
+
+    
 } 
